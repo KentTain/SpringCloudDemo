3e36afa350734db96366d3a686a20984e407fbc2
KentTain <tianchangjun@outlook.com>
2019-10-14 17:10:07 +0800

    add Hystrix project

--- 2.SpringCloud-Hystrix/SpringCloud-HystrixConsumer/src/main/java/com/example/SpringBootConsulApplication.java
+++ 2.SpringCloud-Hystrix/SpringCloud-HystrixConsumer/src/main/java/com/example/SpringBootConsulApplication.java
@@ -2,9 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @SpringBootApplication
 public class SpringBootConsulApplication {
 
---
+++ 2.SpringCloud-Hystrix/SpringCloud-HystrixProvider/src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.User;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public @ResponseBody User hello(@RequestParam(value = "id") int id) {
+        return new User(id, "hello consul");
+    }
+}
