1d40c32ff9f82159e0a01154eea096f6f12bf10e
KentTain <tianchangjun@outlook.com>
2019-10-14 14:03:12 +0800

    finish consul demo

---
+++ 1.SpringCloud-Consul/SpringCloud-ConsulConsumer/src/main/java/com/example/controller/ServiceController.java
@@ -0,0 +1,54 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.model.User;
+
+@RestController
+public class ServiceController {
+
+	@Autowired
+	private LoadBalancerClient loadBalancer;
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	/**
+	 * 获取所有服务
+	 */
+	@RequestMapping("/services")
+	public Object services() {
+		return discoveryClient.getInstances("service-provider");
+	}
+
+	/**
+	 * 从所有服务中选择一个服务（轮询）
+	 */
+	@RequestMapping("/discover")
+	public Object discover() {
+		return loadBalancer.choose("service-provider").getUri().toString();
+	}
+
+	/**
+	 * 调用远程接口
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/call")
+	public User call() {
+		ServiceInstance serviceInstance = loadBalancer.choose("service-provider");
+		System.out.println("服务地址：" + serviceInstance.getUri());
+		System.out.println("服务名称：" + serviceInstance.getServiceId());
+
+		User callServiceResult = new RestTemplate().getForObject(serviceInstance.getUri().toString() + "/hello",
+				User.class);
+		System.out.println(callServiceResult);
+		return callServiceResult;
+	}
+
+}
--- 1.SpringCloud-Consul/SpringCloud-ConsulProvider/src/main/java/com/example/controller/HelloController.java
+++ 1.SpringCloud-Consul/SpringCloud-ConsulProvider/src/main/java/com/example/controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.User;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String hello() {
-        return "hello consul";
+    public @ResponseBody User hello() {
+        return new User(1, "hello consul");
     }
 }
