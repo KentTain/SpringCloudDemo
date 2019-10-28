ee88c7ab1f0158157c089df87d07f19dd96ddc28
KentTain <tianchangjun@outlook.com>
2019-10-28 17:58:16 +0800

    HystrixCollapser

--- 2.SpringCloud-Hystrix/SpringCloud-HystrixConsumer/src/main/java/com/example/service/HelloRemoteFallbackService.java
+++ 2.SpringCloud-Hystrix/SpringCloud-HystrixConsumer/src/main/java/com/example/service/HelloRemoteFallbackService.java
@@ -1,8 +1,22 @@
 package com.example.service;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Future;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.model.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCollapser;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Component
 public class HelloRemoteFallbackService implements HelloRemoteService {
@@ -11,6 +25,29 @@ public class HelloRemoteFallbackService implements HelloRemoteService {
     {
 		System.out.println("调用服务失败--hello");
 		return null;
+    }
+	
+	@Autowired
+	private LoadBalancerClient loadBalancer;
+	
+	@HystrixCollapser(batchMethod = "findAll",
+            collapserProperties = {@HystrixProperty(name = "timerDelayInMilliseconds", value = "100")})
+    public Future<User> find(Long id) {
+        throw new RuntimeException("This method body should not be executed");
+    }
+
+    @HystrixCommand
+    public List<User> findAll(List<Long> ids) {
+        System.out.println("Annotation---------" + ids + "Thread.currentThread().getName():" + Thread.currentThread().getName());
+        ServiceInstance serviceInstance = loadBalancer.choose("service-provider");
+
+		Map<String,String> map = new HashMap<String, String>();
+        map.put("id", String.valueOf(ids));
+		
+		User[] users = new RestTemplate().getForObject(serviceInstance.getUri().toString() + "/hello/users?id=" + String.valueOf(ids),
+				User[].class);
+
+        return Arrays.asList(users);
     }
 	
 	@Override
--- 2.SpringCloud-Hystrix/SpringCloud-HystrixConsumer/src/main/java/com/example/service/HelloRemoteService.java
+++ 2.SpringCloud-Hystrix/SpringCloud-HystrixConsumer/src/main/java/com/example/service/HelloRemoteService.java
@@ -1,5 +1,8 @@
 package com.example.service;
 
+import java.util.List;
+import java.util.concurrent.Future;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +10,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.model.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCollapser;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Component
 @FeignClient(name= "service-provider", fallback = HelloRemoteFallbackService.class)
 public interface HelloRemoteService {
 	@RequestMapping(value = "/hello/index")
     public User hello(@RequestParam(value = "id") int id);
+	
 	
 	@RequestMapping(value = "/hello/timeout", method = RequestMethod.GET)
     public String timeout();
